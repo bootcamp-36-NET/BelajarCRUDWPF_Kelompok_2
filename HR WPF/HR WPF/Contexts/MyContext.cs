d24996a32f788a61c55d25f29457e670c1b7aa81
sebayang <ibrahim.sebayang@gmail.com>
2020-08-16 21:46:32 +0700

    Init All Table

---
+++ HR WPF/HR WPF/Contexts/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using HR_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_WPF.Contexts
+{
+    class MyContext : DbContext
+    {
+        public MyContext(): base("HRWPF") { }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<TransactionItem> TransactionItem { get; set; }
+
+    }
+}
