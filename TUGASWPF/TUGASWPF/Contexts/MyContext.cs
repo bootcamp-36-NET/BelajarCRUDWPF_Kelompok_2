056967316b4e3d76495717d3f29da74a34ff6687
Jepri <55873259+jeffreymas@users.noreply.github.com>
2020-08-17 04:26:57 +0700

    Transaction-TransactionItem

---
+++ TUGASWPF/TUGASWPF/Contexts/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUGASWPF.Model;
+
+namespace TUGASWPF.Contexts
+{
+    class MyContext : DbContext
+    {
+        public  MyContext() : base("TUGASWPF")
+        {
+
+        }
+        public DbSet<Supplier> Supplier { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<TransactionItem> TransactionItem { get; set; }
+
+    }
+}
