ece7b7b0e0588c8be5a888d59bfcb379d675dba9
sebayang <ibrahim.sebayang@gmail.com>
2020-08-17 03:02:32 +0700

    Repleace Project

---
+++ TUGASWPF/TUGASWPF/Contexts/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUGASWPF.Models;
+
+namespace TUGASWPF.Contexts
+{
+    class MyContext : DbContext
+    {
+        public MyContext() : base("TUGASWPF") { }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ TUGASWPF/TUGASWPF/Models/Supplier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGASWPF.Models
+{
+    [Table("tb_m_supplier")]
+    class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? JoinDate { get; set; }
+
+        public Supplier()
+        {
+
+        }
+        public Supplier( string name)
+        { 
+            this.Name = name;
+        }
+        public Supplier(int id, string name, DateTime joinDate)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.JoinDate = joinDate;
+        }
+    }
+    
+}
