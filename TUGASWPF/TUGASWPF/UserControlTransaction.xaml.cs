76477d01a074aa68fee802c5afa15eaa03fc6751
Jepri <55873259+jeffreymas@users.noreply.github.com>
2020-08-18 00:07:33 +0700

    Tambahan

--- TUGASWPF/TUGASWPF/UserControlTransaction.xaml.cs
+++ TUGASWPF/TUGASWPF/UserControlTransaction.xaml.cs
@@ -12,17 +12,134 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TUGASWPF.Contexts;
+using TUGASWPF.Model;
 
 namespace TUGASWPF
 {
     /// <summary>
     /// Interaction logic for UserControlTransaction.xaml
     /// </summary>
+    
     public partial class UserControlTransaction : UserControl
     {
+        MyContext myContext = new MyContext();
         public UserControlTransaction()
         {
             InitializeComponent();
+            dgTransaction.ItemsSource = myContext.Suppliers.ToList();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtId.Text.Equals(""))
+            {
+                MessageBox.Show("Input cant be null");
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure want to Insert this data ?", "Insert Arlet!!!", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        var input = new Transaction(txtId.Text, DpOrderDate.SelectedDate);
+                        myContext.Transaction.Add(input);
+                        myContext.SaveChanges();
+                        MessageBox.Show("1 row has benn inserted");
+                        txtId.Text = "";
+
+                        dgTransaction.ItemsSource = myContext.Transaction.ToList();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+
+            }
+        }
+
+
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure want to Update this data ?", "Update Arlet!!!", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    int Id = Convert.ToInt32(txtId.Text);
+                    var transaction = myContext.Transaction.Find(Id);
+                    myContext.SaveChanges();
+                    MessageBox.Show("1 row has been updated");
+                    dgTransaction.ItemsSource = myContext.Transaction.ToList();
+                    txtId.Text = "";
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure want to delete this data ?", "Delete Arlet!!!", MessageBoxButton.YesNo);
+            var ii = Convert.ToInt32(txtId.Text);
+            var items = myContext.Item.Where(I => I.Transaction.Id == ii).ToList();
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    foreach (Item i in items)
+                    {
+                        int iid = i.Id;
+                        var item = myContext.Item.Find(iid);
+                        myContext.Item.Remove(item);
+                        myContext.SaveChanges();
+                    }
+                    int Id = Convert.ToInt32(txtId.Text);
+                    var supplier = myContext.Transaction.Find(Id);
+                    myContext.Transaction.Remove(supplier);
+                    myContext.SaveChanges();
+                    MessageBox.Show("1 row has been deleted");
+                    dgTransaction.ItemsSource = myContext.Transaction.ToList();
+                    txtId.Text = "";
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+
+        }
+
+        private void txtId_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void dgTransaction_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgTransaction.SelectedItem != null)
+            {
+                var transaction = dgTransaction.SelectedItem as Supplier;
+                txtId.Text = Convert.ToString(transaction.Id);
+                DpOrderDate.SelectedDate = transaction.OrderDate;
+            }
+        }
+
+        private void TabablzControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var filteredData = myContext.Transaction.Where(Q => Q.Id.ToString().Contains(txtSearch.Text) );
+            dgTransaction.ItemsSource = filteredData;
+        }
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            txtId.Text = "";
         }
     }
 }
--- TUGASWPF/TUGASWPF/UserControlTransactionItem.xaml.cs
+++ TUGASWPF/TUGASWPF/UserControlTransactionItem.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TUGASWPF.Contexts;
+using TUGASWPF.Models;
 
 namespace TUGASWPF
 {
@@ -20,9 +22,155 @@ namespace TUGASWPF
     /// </summary>
     public partial class UserControlTransactionItem : UserControl
     {
+        MyContext myContext = new MyContext();
         public UserControlTransactionItem()
         {
             InitializeComponent();
+            dgTransactionItem.ItemsSource = myContext.TransactionItem.ToList();
+            cbTransaction.ItemsSource = myContext.Transaction.ToList();
+            cbItem.ItemsSource = myContext.Item.ToList();
+        }
+
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            var transaction = myContext.TransactionItem.Find(Convert.ToInt32(cbTransaction));
+            var item = myContext.Item.Find(Convert.ToInt32(cbTransaction));
+            if (txtQuantity.Text.Equals("") || transaction == null || item == null)
+            {
+                MessageBox.Show("Input cant be null");
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure want to Insert this data ?", "Insert Arlet!!!", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        var input = new TransactionItem(txtQuantity.Text, transaction, item);
+                        myContext.TransactionItem.Add(input);
+                        myContext.SaveChanges();
+                        MessageBox.Show("1 row has benn inserted");
+
+                        dgTransactionItem.ItemsSource = myContext.TransactionItem.ToList();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+
+            }
+
+        }
+
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBoxResult result = MessageBox.Show("Are you sure want to Update this data ?", "Update Arlet!!!", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    var item = myContext.Item.Find(Convert.ToInt32(cbItem));
+                    var transaction = myContext.Transaction.Find(Convert.ToInt32(cbTransaction));
+                    int Id = Convert.ToInt32(txtId.Text);
+                    var TransactionItem = myContext.TransactionItem.Find(Id);
+                    TransactionItem.Quantity = Convert.ToInt32(txtQuantity.Text);
+                    TransactionItem.Transaction = transaction;
+                    TransactionItem.Item = item;
+                    myContext.SaveChanges();
+                    MessageBox.Show("1 row has been updated");
+                    dgTransactionItem.ItemsSource = myContext.TransactionItem.ToList();
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Wrong Input Data");
+                    break;
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure want to delete this data ?", "Delete Arlet!!!", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    int Id = Convert.ToInt32(txtId.Text);
+                    var transactionitem = myContext.TransactionItem.Find(Id);
+                    myContext.Item.Remove(transactionitem);
+                    myContext.SaveChanges();
+                    MessageBox.Show("1 row has been deleted");
+                    dgTransactionItem.ItemsSource = myContext.TransactionItem.ToList();
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+
+        }
+
+        private void txtId_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void dgTransactionItem_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var transactionitem = dgTransactionItem.SelectedItem as TransactionItem;
+            if (dgTransactionItem.SelectedItem != null)
+            {
+                if (transactionitem.Item == null)
+                {
+
+                    txtQuantity.Text = Convert.ToString(transactionitem.Quantity);
+                    txtId.Text = Convert.ToString(transactionitem.Id);
+                }
+                else
+                {
+                    txtQuantity.Text = Convert.ToString(transactionitem.Quantity);
+                    txtId.Text = Convert.ToString(transactionitem.Id);
+                    cbTransaction.Text = transactionitem.Transaction.Id;
+                }
+            }
+        }
+
+        private void TabablzControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var filteredData = myContext.TransactionItem.Where(Q => Q.Id.ToString().Contains(txtSearch.Text) || Q.Quantity.ToString().Contains(txtSearch.Text)).ToList();
+            dgTransactionItem.ItemsSource = filteredData;
+        }
+
+
+        private void txtPrice_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtStock_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtSupplier_Id_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void cbTransaction_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cbTransaction = Convert.ToInt32(cbTransaction.SelectedValue.ToString());
+        }
+
+        private void cbItem_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cbItem = Convert.ToInt32(cbItem.SelectedValue.ToString());
         }
     }
 }
